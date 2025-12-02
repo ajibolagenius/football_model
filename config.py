b28e8ad90d8cae15c9b18a5d22b115666f991b8a
AJIBOLA (DON_GENIUS) AKELEBE <ajiboladolapogenius@gmail.com>
2025-12-02 17:06:13 +0100

    feat: Integrate dotenv for environment variable loading, update SUPABASE_URL, and add RAPIDAPI_HOST.

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-SUPABASE_URL = "postgresql://postgres:SupabaseOracle@db.xyrqkttjzuuykbwwaafk.supabase.co:5432/postgres"
+load_dotenv()
+
+SUPABASE_URL = "postgresql://postgres.xyrqkttjzuuykbwwaafk:SupabaseOracle@aws-1-eu-central-1.pooler.supabase.com:6543/postgres"
 
 DB_CONNECTION = os.getenv("DATABASE_URL", SUPABASE_URL)
 
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY", "")
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY", "") 
+RAPIDAPI_HOST = "api-football-v1.p.rapidapi.com"
